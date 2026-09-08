a282cee47b07a061b30d4e80692c7cd232a9a743
Maffi-Devi <bhallmaffi@gmail.com>
2026-09-08 11:16:11 +0530

    Bind Flask server to Render port

--- app.py
+++ app.py
@@ -37,4 +37,8 @@ with app.app_context():
     conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=os.environ.get('FLASK_DEBUG', '').lower() == 'true'
+    )
